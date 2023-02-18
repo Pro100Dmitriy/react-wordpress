f13d945a7de686050b039027209a34ca3a2f058f
Dmitry Shestak <dimonshestak@gmail.com>
2023-02-18 14:10:05 +0300

    Add progress

--- build/blocks/section-404/index.asset.php
+++ build/blocks/section-404/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => 'a2b93525b14ca11f1410');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '4a733bb29d9049e05231');
--- functions.php
+++ functions.php
@@ -6,6 +6,24 @@ function init_theme() {
 	require_once( 'lib/acf/theme-options.php' );
 
 	register_block_type( __DIR__ . '/build/blocks/section-error/block.json' );
+	register_block_type( __DIR__ . '/build/blocks/rich-text/block.json' );
+	register_block_type( __DIR__ . '/build/blocks/gutenpride/block.json' );
+	register_block_type( __DIR__ . '/build/blocks/supports/block.json' );
+
+	$asset_file = include( __DIR__ . '/build/blocks/dynamic-block/index.asset.php' );
+
+	wp_register_script(
+		'gutenberg-examples-dynamic',
+		plugins_url( 'build/block.js', __FILE__ ),
+		$asset_file['dependencies'],
+		$asset_file['version']
+	);
+
+	register_block_type( 'gutenberg-examples/example-dynamic', array(
+		'api_version' => 2,
+		'editor_script' => 'gutenberg-examples-dynamic',
+		'render_callback' => 'gutenberg_examples_dynamic_render_callback'
+	) );
 }
 add_action( 'after_setup_theme', 'init_theme' );
 
@@ -20,3 +38,24 @@ function create_block_block_init() {
 //	wp_enqueue_style( 'react-wordpress-style', get_template_directory_uri() . '/assets/section-404.css' );
 }
 add_action( 'wp_enqueue_scripts', 'create_block_block_init' );
+
+/**
+ * Dynamic Block
+ */
+
+function gutenberg_examples_dynamic_render_callback( $block_attributes, $content ) {
+	$recent_posts = wp_get_recent_posts( array(
+		'numberposts' => 1,
+		'post_status' => 'publish',
+	) );
+	if ( count( $recent_posts ) === 0 ) {
+		return 'No posts';
+	}
+	$post = $recent_posts[ 0 ];
+	$post_id = $post['ID'];
+	return sprintf(
+		'<a class="wp-block-my-plugin-latest-post" href="%1$s">%2$s</a>',
+		esc_url( get_permalink( $post_id ) ),
+		esc_html( get_the_title( $post_id ) )
+	);
+}
